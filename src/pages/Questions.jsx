f120a9bdd8d13c6a8772020c3aa236444286f326
20013852 <20013852@student.riverviewschools.com>
2025-10-03 13:26:02 -0400

    Update Questions.jsx fix link
    ln176

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -173,7 +173,7 @@ export default function Questions() {
         ) : (
           <div className="grid gap-6">
             {filteredQuestions.map((question) => (
-              <Link key={question.id} to={createPageUrl(`Question?id=${question.id}`)}>
+              <Link key={question.id} to={createPageUrl(`/Question?id=${question.id}`)}>
                 <Card className="hover:shadow-xl transition-all duration-300 cursor-pointer">
                   <CardHeader>
                     <div className="flex justify-between items-start mb-3">
@@ -215,4 +215,5 @@ export default function Questions() {
       </div>
     </div>
   );
+
 }
