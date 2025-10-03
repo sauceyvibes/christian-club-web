23ba91f618194a529e9ec6b316dd70a019a0b0dc
20013852 <20013852@student.riverviewschools.com>
2025-10-03 09:40:03 -0400

    Update all.js
    change select a catagory string to ln 248

--- src/components/ui/all.js
+++ src/components/ui/all.js
@@ -245,11 +245,11 @@ export const SelectTrigger = ({
   );
 };
 
-export const SelectValue = ({ placeholder, value, children }) => {
+export const SelectValue = ({ placeholder="select a catagory", value, children }) => {
   // This component receives the actual display label from parent
   return (
     <span className={!value ? "text-gray-500" : ""}>
-      {children || value || placeholder="select catagory"}
+      {children || value || placeholder}
     </span>
   );
 };
