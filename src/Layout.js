4b5a5d8dca8240b3aa68f71ae4b5df5bbfb273ce
sauceyvibes <kaicrodden@gmail.com>
2025-10-04 14:56:29 -0400

    fix locations and calls

--- src/Layout.js
+++ src/Layout.js
@@ -27,11 +27,7 @@ export default function Layout({ children }) {
     }
   ];
 
-
-  function Layout() {
-  const { pathname } = useLocation();
   const isForums = pathname.startsWith("/Forums");
-  }
 
   const titleGradient = isForums
   ? "bg-gradient-to-r from-green-600 to-emerald-700" // Forums color
