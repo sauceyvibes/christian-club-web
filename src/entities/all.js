c47804b73b41d80048058435bd831e6fbf2d25d9
sauceyvibes <kaicrodden@gmail.com>
2025-10-07 15:33:49 -0400

    add config

--- src/entities/all.js
+++ src/entities/all.js
@@ -1,3 +1,4 @@
 
 // Simple localStorage-based entities for deployment
+// Simply re-export from Firebase entities
 export { Question, Answer, ForumPost, ForumReply } from '../firebase/entities';
---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyASoKOr-avqgF8UVAQvSg4mBdbSpVW9R0c",
+  authDomain: "downriverchristianquestion-web.firebaseapp.com",
+  projectId: "downriverchristianquestion-web",
+  storageBucket: "downriverchristianquestion-web.firebasestorage.app",
+  messagingSenderId: "27365996491",
+  appId: "1:27365996491:web:c2c7b87e142e54a807e84e",
+  measurementId: "G-TSRMZZLMK5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
